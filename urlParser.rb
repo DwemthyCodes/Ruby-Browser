b79d6e850509d0914fa1f06e88c24b96cd3e8ef2
A <ittoknow@gmail.com>
2010-02-17 22:47:39 -0600

    Added check for trailing / path on url

--- urlParser.rb
+++ urlParser.rb
@@ -15,17 +15,36 @@ class URLParser
 	end
 
 	def format( url )
-		if url !~ /http:\/\//
-			if url !~ /www./
-				url = "http://www." + url
-			else
-				url = "http://" + url
-			end
-		elsif url !~ /www./
-			url.sub!( /http:\/\//, "http://www." )
-		end
-puts url
-		url
+		formatted_url = ensure_http_and_www( url )
+		formatted_url = ensure_path( formatted_url )
+
+		formatted_url
 	end
 
+
+	def ensure_http_and_www( url )
+		if url !~ /http:\/\//
+			if url !~ /www\./
+				retval = "http://www." + url
+			else
+				retval = "http://" + url
+			end
+		elsif url !~ /www\./
+			retval = url.sub( /http:\/\//, "http://www." )
+		else
+			retval = url.dup
+		end
+		
+		retval
+	end
+
+	def ensure_path( url )
+		if url !~ /http:\/\/www\.*\//
+			retval = url + "/"
+		else
+			retval = url.dup
+		end
+
+		retval
+	end
 end
